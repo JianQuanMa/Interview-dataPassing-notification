46f2a3a395f27a2af27ece6d5037676c783e6058
Jian Ma <jianquanma@gmail.com>
2020-10-07 17:15:26 -0400

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Interview-dataPassing-Notification
+//
+//  Created by Jian Ma on 10/7/20.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
+
+
+
+
