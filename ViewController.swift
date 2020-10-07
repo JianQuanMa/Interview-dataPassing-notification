a08cd358d3c7fb13e197072d7e0f45463308ae51
Jian Ma <jianquanma@gmail.com>
2020-10-07 17:17:06 -0400

    some changes

--- ViewController.swift
+++ ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //some changes
     }
 
 
