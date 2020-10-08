d71183c320503aaf7a6dede6a6f0b857877cb9a4
Jian Ma <jianquanma@gmail.com>
2020-10-08 15:46:51 -0400

    added new slectTimeViewController

---
+++ SelectTimeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SelectTimeViewController.swift
+//  Interview-dataPassing-Notification
+//
+//  Created by Jian Ma on 10/8/20.
+//
+
+import UIKit
+
+
+class SelectTimeViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
