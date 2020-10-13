ae4fe161630c8e63da43833e2fdcae3c55dd59dc
Jian Ma <jianquanma@gmail.com>
2020-10-13 01:01:52 -0400

    added auto turn-off the observer when leaving a viewController functionality

--- SecondViewController.swift
+++ SecondViewController.swift
@@ -9,12 +9,28 @@ import UIKit
 
 class SecondViewController: UIViewController{
     @IBOutlet weak var dateLabel: UILabel!
+    var observer: NSObjectProtocol?
     override func viewDidLoad() {
         super.viewDidLoad()
-        NotificationCenter.default.addObserver(forName: .saveDateTime, object: nil, queue: OperationQueue.main) { (notification) in
+//        NotificationCenter.default.addObserver(forName: .saveDateTime, object: nil, queue: OperationQueue.main) { (notification) in
+//            let dateVC = notification.object as! DatePopupViewController
+//            self.dateLabel.text = dateVC.formattedDate
+//        }
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //new way
+        observer = NotificationCenter.default.addObserver(forName: .saveDateTime, object: nil, queue: OperationQueue.main) { (notification) in
             let dateVC = notification.object as! DatePopupViewController
             self.dateLabel.text = dateVC.formattedDate
         }
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        if let observer = observer {
+            NotificationCenter.default.removeObserver(observer)
+        }
+        
     }
     
 }
--- ViewController.swift
+++ ViewController.swift
@@ -10,17 +10,31 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var dateLabel: UILabel!
+    var observer: NSObjectProtocol?
     override func viewDidLoad() {
         super.viewDidLoad()
         //old way
         //the commented method is the first notification observer method completed by a selector method, as supposed what we have now, the completion handler style
         //set up the notification lstiener
      //   NotificationCenter.default.addObserver(self, selector: #selector(handlePopupClosing), name: .saveDateTime, object: nil)
+        
+       
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         //new way
-        NotificationCenter.default.addObserver(forName: .saveDateTime, object: nil, queue: OperationQueue.main) { (notification) in
+        observer = NotificationCenter.default.addObserver(forName: .saveDateTime, object: nil, queue: OperationQueue.main) { (notification) in
             let dateVC = notification.object as! DatePopupViewController
             self.dateLabel.text = dateVC.formattedDate
         }
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        if let observer = observer {
+            NotificationCenter.default.removeObserver(observer)
+        }
+        
     }
 
     //Mark: helper
