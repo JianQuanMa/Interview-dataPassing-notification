b929ca07a919e8591e8014ac0a703c09069c0e6a
Jian Ma <jianquanma@gmail.com>
2020-10-30 22:27:12 -0400

    added retain cycle problems for callback view controller and fix it with three methods, 1)set weak popup,2)capture list and 3)manually set popup to nil in closure

--- SecondViewController.swift
+++ SecondViewController.swift
@@ -10,38 +10,43 @@ import UIKit
 class SecondViewController: UIViewController{
     @IBOutlet weak var dateLabel: UILabel!
     var observer: NSObjectProtocol?
+    // first option to break cycle is to set popup reference to weak
+    weak var popup: DatePopupViewController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //method 1: notification normal observer
-//        NotificationCenter.default.addObserver(forName: .saveDateTime, object: nil, queue: OperationQueue.main) { (notification) in
-//            let dateVC = notification.object as! DatePopupViewController
-//            self.dateLabel.text = dateVC.formattedDate
-//        }
+        //        NotificationCenter.default.addObserver(forName: .saveDateTime, object: nil, queue: OperationQueue.main) { (notification) in
+        //            let dateVC = notification.object as! DatePopupViewController
+        //            self.dateLabel.text = dateVC.formattedDate
+        //        }
     }
     //method2: notification observer completion handler style
-//    override func viewDidAppear(_ animated: Bool) {
-//        super.viewDidAppear(animated)
-//        //new way
-//        observer = NotificationCenter.default.addObserver(forName: .saveDateTime, object: nil, queue: OperationQueue.main) { (notification) in
-//            let dateVC = notification.object as! DatePopupViewController
-//            self.dateLabel.text = dateVC.formattedDate
-//        }
-//    }
-//    override func viewDidDisappear(_ animated: Bool) {
-//        super.viewDidDisappear(animated)
-//        if let observer = observer {
-//            NotificationCenter.default.removeObserver(observer)
-//        }
-//
-//    }
+    //    override func viewDidAppear(_ animated: Bool) {
+    //        super.viewDidAppear(animated)
+    //        //new way
+    //        observer = NotificationCenter.default.addObserver(forName: .saveDateTime, object: nil, queue: OperationQueue.main) { (notification) in
+    //            let dateVC = notification.object as! DatePopupViewController
+    //            self.dateLabel.text = dateVC.formattedDate
+    //        }
+    //    }
+    //    override func viewDidDisappear(_ animated: Bool) {
+    //        super.viewDidDisappear(animated)
+    //        if let observer = observer {
+    //            NotificationCenter.default.removeObserver(observer)
+    //        }
+    //
+    //    }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "toDatePopupViewControllerSegue" {
-            let popup = segue.destination as! DatePopupViewController
-            popup.shouldShowTimePicker = false
+            popup = segue.destination as! DatePopupViewController
+            popup?.shouldShowTimePicker = false
             //assign to a func
-            popup.onSave = { [weak self] (data) in
+            // second option is to add a captur elist
+            popup?.onSave = { [weak self] (data) in
                 self?.dateLabel.text = data
+            //third way is to manually set popup to nil
+                //popup = nil
             }
         }
     }
@@ -50,4 +55,9 @@ class SecondViewController: UIViewController{
         dateLabel.text = data
     }
     
+    deinit {
+        print("secondViewController is deinited")
+    }
 }
+
+
